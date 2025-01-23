3908f1c5967b0e83c41b92226a4163338b295cda
calloc134 <115209243+calloc134@users.noreply.github.com>
2025-01-23 18:16:20 +0900

    適当に

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-use std::fmt::Debug;
-
 use generate_anchor_page_url::generate_anchor_page_url;
 use parse_metadata::extract_metadata;
 use proconio::input;
@@ -10,6 +8,7 @@ use ureq::agent;
 
 fn main() {
     // まずURLを取得
+    println!("Please input the URL of the target page");
     input! {
         url: String,
     }
@@ -26,10 +25,10 @@ fn main() {
     let response = response.unwrap();
     let url = response.get_url().to_string();
     let html = response.into_string().unwrap();
-    let cookies = client.cookie_store();
+    // let cookies = client.cookie_store();
     println!("URL: {}", url);
-    let cookies_hoge = cookies.iter_any().collect::<Vec<_>>();
-    println!("Cookies: {:?}", cookies_hoge);
+    // let cookies_hoge = cookies.iter_any().collect::<Vec<_>>();
+    // println!("Cookies: {:?}", cookies_hoge);
 
     // メタデータを取得
     let metadata = extract_metadata(&html).unwrap();
@@ -47,7 +46,7 @@ fn main() {
         //     'Wicket-Ajax-BaseURL': '{}'.format(base_url)
         // }
 
-        print!("Accessing the anchor page URL...");
+        println!("Accessing the anchor page URL");
 
         // アンカーページにアクセス
         let response = client
@@ -57,23 +56,42 @@ fn main() {
             .set("Wicket-Ajax-BaseURL", &url)
             .call();
 
-        print!("Accessed the anchor page URL");
-
         if (response.is_err()) {
             panic!("Failed to access the anchor page URL");
         }
 
         let response = response.unwrap();
         let html = response.into_string().unwrap();
-        print!("HTML: {}", html);
+        // print!("HTML: {}", html);
 
         // アンカーページのレスポンスの解析
-        let page_image_url = parse_image_url::get_page_image_url(&html).unwrap();
+        // let page_image_url = parse_image_url::get_page_image_url(&html).unwrap_or_else(|| {
+        //     println!("Failed to parse the page image URL");
+        //     println!("HTML: {}", html);
+        //     panic!("Failed to parse the page image URL");
+        // });
+
+        let page_image_url = match parse_image_url::get_page_image_url(&html) {
+            Ok(url) => url,
+            Err(e) => {
+                println!("Failed to parse the page image URL: {:?}", e);
+                println!("HTML: {}", html);
+                panic!("Failed to parse the page image URL");
+            }
+        };
 
         println!("Page image URL: {}", page_image_url);
 
+        // すこし待機
+        std::thread::sleep(std::time::Duration::from_secs(0.5));
+
         // ファイルのダウンロード
-        // とりあえずダミーの処理
+        let response = client.get(&page_image_url).call();
+        if (response.is_err()) {
+            panic!("Failed to download the image file");
+        }
+
+        let response = response.unwrap();
     }
 
     println!("Done");
--- src/parse_image_url.rs
+++ src/parse_image_url.rs
@@ -33,30 +33,3 @@ pub fn get_page_image_url(html: &str) -> Result<String> {
 
     Ok(text_content)
 }
-
-fn main() -> Result<()> {
-    let html = r#"<?xml version="1.0" encoding="UTF-8"?><ajax-response><component id="id18b" ><![CDATA[<div id="id18b">
-        <span name="_scale" style="display:none;" id="id18c">1</span>
-        <span name="_pageNum" style="display:none;" id="id18d">1</span>
-        <span name="_pageImageURL" style="display:none;" id="id18e">/kikan_info/GetImg.jpg?id=119323131220803976491832</span>
-        <span name="_pageImageScale" style="display:none;" id="id18f">200</span>
-        <span name="_pageServiceMsg" style="display:none;" id="id190"></span>
-        <span name="_isDrawOnePage" style="display:none;" id="id191">true</span>
-        <span name="_isLeftToRightPage" style="display:none;" id="id192">true</span>
-        <script type="text/javascript">
-/*<![CDATA[*/
-
-          readParams();
-          drawPageImage();
-        
-/*]]]]><![CDATA[>*/
-</script>
-      </div>]]></component><priority-evaluate><![CDATA[(function(){setCurrentScale();})();]]></priority-evaluate><evaluate><![CDATA[(function(){startLoadingIndicator();})();]]></evaluate></ajax-response>"#;
-
-    match get_page_image_url(html) {
-        Ok(url) => println!("_pageImageURL: {}", url),
-        Err(e) => println!("Error: {}", e),
-    }
-
-    Ok(())
-}
