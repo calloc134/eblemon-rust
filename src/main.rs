2c1301a2f96eb11f7ec0bfe1c1dc8af97185d3fc
calloc134 <115209243+calloc134@users.noreply.github.com>
2025-01-23 18:31:58 +0900

    適当やね

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,55 @@
 use generate_anchor_page_url::generate_anchor_page_url;
 use parse_metadata::extract_metadata;
-use proconio::input;
 mod generate_anchor_page_url;
 mod parse_image_url;
 mod parse_metadata;
+use dialoguer::Input;
+use log::{debug, error, info};
 use ureq::agent;
 
 fn main() {
+    // ロガーの初期化
+    env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
     // まずURLを取得
-    println!("Please input the URL of the target page");
-    input! {
-        url: String,
-    }
+    // info!("Please input the URL of the target page");
+    // input! {
+    //     url: String,
+    // }
+    let url = Input::<String>::new()
+        .with_prompt("Please input the URL of the target page")
+        .interact()
+        .unwrap_or_else(|e| {
+            error!("Failed to get the URL: {:?}", e);
+            panic!("Failed to get the URL");
+        });
 
     // セッションを作成
     let client = agent();
+    info!("Successfully created a session. Start accessing the URL");
 
     // アクセス
-    let response = client.get(&url).call();
-    if (response.is_err()) {
+    let response = client.get(&url).call().unwrap_or_else(|e| {
+        error!("Failed to access the URL: {:?}", e);
         panic!("Failed to access the URL");
-    }
+    });
 
-    let response = response.unwrap();
+    info!("Successfully accessed. Start extracting metadata");
+
     let url = response.get_url().to_string();
     let html = response.into_string().unwrap();
-    // let cookies = client.cookie_store();
-    println!("URL: {}", url);
-    // let cookies_hoge = cookies.iter_any().collect::<Vec<_>>();
-    // println!("Cookies: {:?}", cookies_hoge);
 
     // メタデータを取得
     let metadata = extract_metadata(&html).unwrap();
-    println!("Title: {}", metadata.title);
-    println!("Total pages: {}", metadata.total_pages);
+    info!(
+        "Title: {}, Total pages: {}",
+        metadata.title, metadata.total_pages
+    );
 
+    info!("Start downloading the image files...");
     for i in 1..=metadata.total_pages {
         // アンカーページのURLを生成
         let anchor_page_url = generate_anchor_page_url(&url, i);
-        println!("Anchor page URL: {}", anchor_page_url);
-
-        // headers = {
-        //     'X-Requested-With': 'XMLHttpRequest',
-        //     'Wicket-Ajax': 'true',
-        //     'Wicket-Ajax-BaseURL': '{}'.format(base_url)
-        // }
-
-        println!("Accessing the anchor page URL");
+        debug!("Anchor page URL: {}", anchor_page_url);
 
         // アンカーページにアクセス
         let response = client
@@ -54,45 +57,41 @@ fn main() {
             .set("X-Requested-With", "XMLHttpRequest")
             .set("Wicket-Ajax", "true")
             .set("Wicket-Ajax-BaseURL", &url)
-            .call();
+            .call()
+            .unwrap_or_else(|e| {
+                error!("Failed to access the anchor page URL: {:?}", e);
+                panic!("Failed to access the anchor page URL");
+            });
 
-        if (response.is_err()) {
-            panic!("Failed to access the anchor page URL");
-        }
-
-        let response = response.unwrap();
         let html = response.into_string().unwrap();
-        // print!("HTML: {}", html);
 
-        // アンカーページのレスポンスの解析
-        // let page_image_url = parse_image_url::get_page_image_url(&html).unwrap_or_else(|| {
-        //     println!("Failed to parse the page image URL");
-        //     println!("HTML: {}", html);
-        //     panic!("Failed to parse the page image URL");
-        // });
+        let page_image_url = parse_image_url::get_page_image_url(&html).unwrap_or_else(|e| {
+            error!("Failed to parse the page image URL: {:?}", e);
+            debug!("HTML: {}", html);
+            panic!("Failed to parse the page image URL");
+        });
 
-        let page_image_url = match parse_image_url::get_page_image_url(&html) {
-            Ok(url) => url,
-            Err(e) => {
-                println!("Failed to parse the page image URL: {:?}", e);
-                println!("HTML: {}", html);
-                panic!("Failed to parse the page image URL");
-            }
-        };
-
-        println!("Page image URL: {}", page_image_url);
+        info!("Page image URL: {}", page_image_url);
 
         // すこし待機
-        std::thread::sleep(std::time::Duration::from_secs(0.5));
+        std::thread::sleep(std::time::Duration::from_millis(500));
 
         // ファイルのダウンロード
-        let response = client.get(&page_image_url).call();
-        if (response.is_err()) {
+        let response = client.get(&page_image_url).call().unwrap_or_else(|e| {
+            error!("Failed to download the image file: {:?}", e);
             panic!("Failed to download the image file");
-        }
+        });
+        let mut output_image =
+            std::fs::File::create(format!("page{}.jpg", i)).unwrap_or_else(|e| {
+                error!("Failed to create the image file: {:?}", e);
+                panic!("Failed to create the image file");
+            });
 
-        let response = response.unwrap();
+        std::io::copy(&mut response.into_reader(), &mut output_image).unwrap_or_else(|e| {
+            error!("Failed to write the image file: {:?}", e);
+            panic!("Failed to write the image file");
+        });
     }
 
-    println!("Done");
+    info!("All image files have been downloaded");
 }
--- src/parse_image_url.rs
+++ src/parse_image_url.rs
@@ -1,4 +1,4 @@
-use anyhow::{anyhow, Context, Result};
+use anyhow::Result;
 use scraper::{Html, Selector};
 use thiserror::Error;
 
